201388d6b2800f2c24a724a10bdb38a64675cc0c
Mitchell-Lunewave <mitchell@lunewave.com>
2025-07-15 10:26:52 -0700

    Update code to read with and without DMA data

--- EVB_Calibration_Analysis/Check_FFT_and_RawData.m
+++ EVB_Calibration_Analysis/Check_FFT_and_RawData.m
@@ -4,12 +4,12 @@
 
 clear all;close all;clc;
 step = 3;
-AZ_FOV = 360;
+AZ_FOV = 30;
 az_steps = AZ_FOV/step + 1;
-EL_FOV = 66;
+EL_FOV = 0;
 el_steps = EL_FOV/step + 1;
-path = 'U:\Falcon_Project\20250617_LWOfficeTest_AZ360_EL66_Step3_withLens_withEVB_2.456GHz_LibraryTest\';
-az_angle = -51;        el_angle = 57;
+path = 'U:\Falcon_Project\20250617_LWOfficeTest_AZ30_EL0_Step3_withLens_withEVB_noise\';
+az_angle = 15;        el_angle = 0;
 
 
 
@@ -37,6 +37,21 @@ C7= C0 (7:8:end,:).'; C_all(:,7)=C7(:);
 C8= C0 (8:8:end,:).'; C_all(:,8)=C8(:); 
 
 C1_cmplex=C_all(1:2:end,:)+1i*C_all(2:2:end,:);
+
+% C = fread(fileID, Inf, 'int16');fclose(fileID);
+% C0 = reshape(C,[8,length(C)/8]).';
+% L_C0=length(C0);
+% 
+% C1= C0 (1:2:L_C0/4,:).'; C_all(:,1)=C1(:);
+% C2= C0 (2:2:L_C0/4,:).'; C_all(:,2)=C2(:);
+% C3= C0 (L_C0/4+1:2:L_C0/4*2,:).'; C_all(:,3)=C3(:);
+% C4= C0 (L_C0/4+2:2:L_C0/4*2,:).'; C_all(:,4)=C4(:);
+% C5= C0 (L_C0/2+1:2:L_C0/4*3,:).'; C_all(:,5)=C5(:);
+% C6= C0 (L_C0/2+2:2:L_C0/4*3,:).'; C_all(:,6)=C6(:);
+% C7= C0 (L_C0/4*3+1:2:L_C0,:).'; C_all(:,7)=C7(:);
+% C8= C0 (L_C0/4*3+2:2:L_C0,:).'; C_all(:,8)=C8(:); 
+% 
+% C1_cmplex=C_all(1:2:end,:)+1i*C_all(2:2:end,:);
 
 
 
